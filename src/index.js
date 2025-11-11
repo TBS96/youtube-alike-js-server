95afabde94b66d8783b0076c885054930893b4b2
TBS96 <9tbs6@proton.me>
2025-11-11 20:53:40 +0530

    call express() in app.js and modify in index.js
    - in `app.js`, initialize `express` app and export it.
    - in `index.js`, add `.then` and `.catch` blocks with `connectDB()` as it returns a promise.
    - import app from `app.js` and at first in `then()` block, check for any app error with 'error' listener with `app.on`
    - listen to port and start server
    - in `.catch()` block, show mongodb connection failed message with `error`
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-// require('dotenv').config({ path: './env' })
-import dotenv from 'dotenv'
-import connectDB from './db/index.js'
+/* SECOND APPROACH (MODULAR) */
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
-    path: './.env'
+  path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on('error', (error) => {
+        console.error(`APP ERROR: ${error}`);
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT: ${process.env.PORT}`);
+    })
+  })
+  .catch((err) => {
+    console.error(`MONGODB CONNECTION FAILED!!!: ${err}`);
+  });
 
 
 
@@ -26,7 +40,10 @@ connectDB();
 
 
 
-/* FIRST APPROACH WITH IIFE, CALLING AND CONNECTING DB WITH ASYNC-AWAIT AND TRY-CATCH, WITH ERROR HANDLING
+
+
+
+  /* FIRST APPROACH WITH IIFE, CALLING AND CONNECTING DB WITH ASYNC-AWAIT AND TRY-CATCH, WITH ERROR HANDLING (NOT MODULAR)
 import mongoose from 'mongoose'
 import { DB_NAME } from './constants'
 import express from 'express'
