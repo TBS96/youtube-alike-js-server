d021386829c2bedcb99759232c72178998ab3a17
TBS96 <9tbs6@proton.me>
2025-11-15 19:25:48 +0530

    add user and video models with authentication methods and pagination support
    - install pkgs: `jsonwebtoken, bcrypt, `mongoose-aggregate-paginate-v2`
    - create models: `User` and `Video` with schemas
    - create `userSchema.pre()` middleware function from mongoose are executed one after another, when each middleware calls 'next'. this function will be executed for encrypting password
    - create custom mongoose method `isPasswordCorect` to compare and check whether the password and its hashed version is same or not using `bcrypt.compare()`. it takes computational power and time to compare, so using async-await.
    - create custom mongoose methods `generateAccessToken` and `generateRefreshToken` to generate access and refresh tokens with 'jwt.sign()', in which 1st object/string/array will have payload(data), 2nd parameter is the secret/private-key, and 3rd is the sign options, basically object to set the expiry of the token.
    - in `video.model.js` import `mongooseAggregatePaginate` and wrap it inside plugin method from `videoSchema` (WILL BE DISCUSSED IT LATER)
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

---
+++ src/models/user.model.js
@@ -0,0 +1,97 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avatar: {
+            type: String,   // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String,   // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Video',
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+        },
+        refreshToken: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+// 'pre' middleware functions from mongoose are executed one after another, when each middleware calls 'next'
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+});
+
+// creating custom mongoose method 'isPasswordCorect' to compare and check whether the password and its hashed version is same or not using 'bcrypt.compare()'. it takes computational power and time to compare, so using async-await.
+userSchema.methods.isPasswordCorect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+
+// creating custom mongoose method 'generateAccessToken' to generate access token with 'jwt.sign()', in which 1st object/string/array will have payload(data), 2nd parameter is the secret/private-key, and 3rd is the sign options, basically object to set the expiry of the token.
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+// creating custom mongoose method 'generateRefreshToken' to generate refresh token with 'jwt.sign()', in which 1st object/string/array will have payload(data), 2nd parameter is the secret/private-key, and 3rd is the sign options, basically object to set the expiry of the token.
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model('User', userSchema);
