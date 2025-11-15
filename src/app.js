451c6972b32d3f15446fa5a340c6b1c2b74a795f
TBS96 <9tbs6@proton.me>
2025-11-15 11:35:04 +0530

    configure `app.js` to use express's built-in + outer middlewares
    - `cors()`: for cross origin resource sharing.
    - `express.json()`: for text forms that will come from client in json.
    - `express.urlencoded()`: for urls, use encoder, because we need to tell express that data may come as params from client side.
    - `express.static()`: its associated with the public folder to keep static assets.
    - `cookieParser`: access/set the cookies in the user's browser from my server. basically i can perform CRUD operations in user's browser. because there are some ways by which i can keep secure cookies in the user's browser, so that the server can read or remove those secure cookies.
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+
+// for text forms that will come from client in json
+app.use(express.json({limit: '16kb'}));
+
+// for urls, use encoder, because we need to tell express that data may come as params from client side
+app.use(express.urlencoded({extended: true, limit: '16kb'}));
+
+// its associated with the public folder to keep static assets
+app.use(express.static('public'));
+
+// access/set the cookies in the user's browser from my server. basically i can perform CRUD operations in user's browser. because there are some ways by which i can keep secure cookies in the user's browser, so that the server can read or remove those secure cookies.
+app.use(cookieParser);
 
 export default app;
 // export { app };
