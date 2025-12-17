363be08160956e2dcad0670e8162089815f039d9
TBS96 <9tbs6@proton.me>
2025-12-17 23:16:52 +0530

    fix: update security options in constants.js for local development
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,13 @@ export const DB_NAME = 'youtube-alike-db';
 
 export const options = {
     httpOnly: true,
-    secure: true,
+    secure: false,
+    sameSite: 'Lax',
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
+
+
+
+// NOTES:
+// for localhost(http), sameSite=Lax, secure=false
+// for production(https), sameSite=None, secure=true
