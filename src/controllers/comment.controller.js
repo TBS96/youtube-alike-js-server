cc127e2b97a515aa2a5ac866b30ea7546162f65b
TBS96 <9tbs6@proton.me>
2026-01-07 21:01:17 +0530

    feat: implement `deleteComment` controller
    _**logic building phase for `deleteComment` in `comment.controller.js`:**_
    
    1. extract `commentId` from `req.params` and validate it
    2. find if the `commentId` exists using `findById` else throw `404` error
    3. check ownership of the commenter using `comment.owner` and `req.user._id`, else throw `403` error
    4. delete `commentId` using `findByIdAndUpdate`
    5. return success response
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -121,7 +121,58 @@ const updateComment = asyncHandler(async (req, res) => {
     // ========== 4. return success response ==========
 });
 
+
+
+const deleteComment = asyncHandler(async (req, res) => {
+    /* ** algorithm to follow step by step, to add a comment by video ID **
+    1. extract commentId from req.params and validate it
+    2. find if the commentId exists using findById else throw 404 error
+    3. check ownership of the commenter using comment.owner and req.user._id, else throw 403 error
+    4. delete commentId using findByIdAndDelete
+    5. return success response
+    */
+
+    // ========== 1. extract commentId from req.params and validate it ==========
+    const { commentId } = req.params;
+    
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, 'Invalid or missing comment ID');
+    }
+    // ========== 1. extract commentId from req.params and validate it ==========
+
+    
+    // ======== 2. find if the commentId exists using findById else throw 404 error ========
+    const comment = await Comment.findById(commentId);
+    
+    if (!comment) {
+        throw new ApiError(404, 'Comment not found');
+    }
+    // ======== 2. find if the commentId exists using findById else throw 404 error ========
+    
+
+    // ========== 3. check ownership of the commenter using comment.owner and req.user._id, else throw 403 error ==========
+    if (comment.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(403, 'Unauthorized! You do not have permission to delete this comment!');
+    }
+    // ========== 3. check ownership of the commenter using comment.owner and req.user._id, else throw 403 error ==========
+    
+    
+    // ======== 4. delete commentId using findByIdAndDelete ========
+    await Comment.findByIdAndDelete(commentId);
+    // ======== 4. delete commentId using findByIdAndDelete ========
+
+    
+    // =========== 5. return success response ===========
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, {}, 'Comment deleted successfully')
+    );
+    // =========== 5. return success response ===========
+});
+
 export {
     addComment,
     updateComment,
+    deleteComment,
 }
